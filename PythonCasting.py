59e340d41f248fb404b3a42972f648fd4fb940b8
Rohit5a3 <104223348+Rohit5a3@users.noreply.github.com>
2022-06-26 15:34:13 +0530

    Add files via upload

---
+++ PythonCasting.py
@@ -0,0 +1,29 @@
+x=-96754
+y="rohit"
+z=complex(x) #complex casting
+print(z)
+print(x,y)
+a=float(x) #float casting
+b=str(x)   #str casting
+print(a,b)
+print(int(a))
+print(type(a),type(b),type(z)) #printing the type cast
+s=3e5434
+e=-3456.4e45
+print(type(s),type(e))
+
+
+
+#---Camelcase:-Each case except the first starts with camelcase
+myNameIsRohit="rohit"
+print(myNameIsRohit)
+#snake case
+MyNameIsRohit="rohit"
+print(MyNameIsRohit)
+a=14.44
+b=13
+z= a or b
+print(z)
+
+
+
---
+++ patternstring.py
@@ -0,0 +1,14 @@
+#reverse the string and get its first letter capital
+st="darshan"
+print(st[::-1].capitalize(),end=' ')
+#swap case
+print(st.swapcase(),end=' ')
+
+
+
+
+
+
+
+
+
